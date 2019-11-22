b48ee26a96936ef6dce41a20b3c15b95897f4875
Maciej Bratek <maclav3@gmail.com>
2019-11-22 05:18:18 +0100

    Just return error, don't log it

--- pkg/sql/publisher.go
+++ pkg/sql/publisher.go
@@ -169,17 +169,15 @@ func (p *Publisher) autocommitWarning() error {
 	adapterDB, adapterWithDB := p.config.SchemaAdapter.(withDBHandle)
 
 	if dbIsTx && !adapterWithDB {
-		p.logger.Info("WARNING: it appears that the publisher's database handle is a transaction and the schema adapter "+
-			"doesn't provide it's own handle. Use a schema adapter that implements the `DB() *sql.DB` method and returns a handle."+
-			"Check the DefaultMySQLSchema adapter implementation for more details.", nil)
-		return errors.New("adapter schema has no separate db handle, danger of implicit commit")
+		return errors.New("it appears that the publisher's database handle is a transaction and the schema adapter " +
+			"doesn't provide it's own handle. Use a schema adapter that implements the `DB() *sql.DB` method and returns a handle." +
+			"Check the DefaultMySQLSchema adapter implementation for more details.")
 	}
 
 	if dbIsTx && adapterDB.DB() == nil {
-		p.logger.Info("WARNING: it appears that the publisher's database handle is a transaction and the schema adapter "+
-			"provides the `DB() *sql.DB` method, but no handle is passed. If using the default adapter, check the WithDB option. "+
-			"Check the DefaultMySQLSchema adapter implementation for more details.", nil)
-		return errors.New("adapter schema has no separate db handle, danger of implicit commit")
+		return errors.New("WARNING: it appears that the publisher's database handle is a transaction and the schema adapter " +
+			"provides the `DB() *sql.DB` method, but no handle is passed. If using the default adapter, check the WithDB option. " +
+			"Check the DefaultMySQLSchema adapter implementation for more details.")
 	}
 	return nil
 }
