e54c60ac95db982f79cc40fcdd2742aa27dcc9ab
Miłosz Smółka <milosz.smolka@gmail.com>
2021-04-05 22:59:05 +0200

    Update pkg/sql/context.go

--- pkg/sql/context.go
+++ pkg/sql/context.go
@@ -15,11 +15,11 @@ func setTxToContext(ctx context.Context, tx *sql.Tx) context.Context {
 	return context.WithValue(ctx, txContextKey, tx)
 }
 
-// TxFromContext returns transaction used by subscriber to consume message.
-// The transaction will be committed if ack of message will be successful.
+// TxFromContext returns the transaction used by the subscriber to consume the message.
+// The transaction will be committed if ack of the message is successful.
 // When a nack is sent, the transaction will be rolled back.
 //
-// It is useful, when you want to ensure that data is update only when message will be processed.
+// It is useful when you want to ensure that data is updated only when the message is processed.
 // Example usage: https://github.com/ThreeDotsLabs/watermill/tree/master/_examples/real-world-examples/exactly-once-delivery-counter
 func TxFromContext(ctx context.Context) (*sql.Tx, bool) {
 	tx, ok := ctx.Value(txContextKey).(*sql.Tx)
