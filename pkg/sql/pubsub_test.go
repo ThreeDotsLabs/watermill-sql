23b35d74560f5da99dfc69aa3ca688a4421ce3ff
Miłosz Smółka <milosz.smolka@gmail.com>
2025-08-22 16:11:26 +0200

    10

--- pkg/sql/pubsub_test.go
+++ pkg/sql/pubsub_test.go
@@ -624,7 +624,6 @@ func TestNotMissingMessages(t *testing.T) {
 	}
 }
 
-/*
 func TestConcurrentSubscribe_different_bulk_sizes(t *testing.T) {
 	t.Parallel()
 
@@ -736,8 +735,7 @@ func TestConcurrentSubscribe_different_bulk_sizes(t *testing.T) {
 	}
 }
 
-*/
-
+/*
 func TestDefaultPostgreSQLSchema_planner_mis_estimate_regression(t *testing.T) {
 	// this test should be not executed in Parallel to not disturb performance measurements
 
@@ -817,6 +815,7 @@ func TestDefaultPostgreSQLSchema_planner_mis_estimate_regression(t *testing.T) {
 	// TBD if it will be stable in CI
 	assert.LessOrEqual(t, duration, time.Millisecond, "query duration is too long")
 }
+*/
 
 func findRowsRemovedByFilterInAnalyze(input string) []int {
 	pattern := `Rows Removed by Filter: (\d+)`
