3b87c0aa2a47116a55efbc77f1cc3b87bdd26c93
Adrian <40515793+AdrianZajkowski@users.noreply.github.com>
2025-10-23 11:31:56 +0200

    add delayed mysql requeuer (#52)
    Co-authored-by: adrian.zajkowski <adrian.zajkowski@nordsec.com>

--- pkg/sql/delayed_requeuer_test.go
+++ pkg/sql/delayed_requeuer_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/ThreeDotsLabs/watermill/message"
 )
 
-func TestDelayedRequeuer(t *testing.T) {
+func TestPostgreSQLDelayedRequeuer(t *testing.T) {
 	t.Parallel()
 
 	db := newPostgreSQL(t)
@@ -47,6 +47,75 @@ func TestDelayedRequeuer(t *testing.T) {
 		func(msg *message.Message) error {
 			payload := string(msg.Payload)
 			if payload == `{"error":true}` {
+				return fmt.Errorf("error")
+			}
+
+			receivedMessages = append(receivedMessages, msg.UUID)
+
+			return nil
+		},
+	)
+
+	go func() {
+		err := router.Run(context.Background())
+		require.NoError(t, err)
+	}()
+
+	<-router.Running()
+
+	go func() {
+		err := delayedRequeuer.Run(context.Background())
+		require.NoError(t, err)
+	}()
+
+	err = publisher.Publish(topic, message.NewMessage("1", []byte(`{}`)))
+	require.NoError(t, err)
+
+	err = publisher.Publish(topic, message.NewMessage("2", []byte(`{"error":true}`)))
+	require.NoError(t, err)
+
+	err = publisher.Publish(topic, message.NewMessage("3", []byte(`{}`)))
+	require.NoError(t, err)
+
+	assert.EventuallyWithT(t, func(t *assert.CollectT) {
+		assert.Equal(t, []string{"1", "3"}, receivedMessages)
+	}, 1*time.Second, 100*time.Millisecond)
+}
+
+func TestMySQLDelayedRequeuer(t *testing.T) {
+	t.Parallel()
+
+	db := newMySQL(t)
+	schemaAdapter := sql.DefaultMySQLSchema{}
+	offsetsAdapter := sql.DefaultMySQLOffsetsAdapter{}
+	publisher, subscriber := newPubSub(t, db, "test", schemaAdapter, offsetsAdapter)
+
+	topic := watermill.NewUUID()
+
+	err := subscriber.(message.SubscribeInitializer).SubscribeInitialize(topic)
+	require.NoError(t, err)
+
+	delayedRequeuer, err := sql.NewMySQLDelayedRequeuer(sql.DelayedRequeuerConfig{
+		DB:           db,
+		RequeueTopic: watermill.NewUUID(),
+		Publisher:    publisher,
+		Logger:       logger,
+	})
+	require.NoError(t, err)
+
+	router := message.NewDefaultRouter(logger)
+	router.AddMiddleware(delayedRequeuer.Middleware()...)
+
+	var receivedMessages []string
+
+	router.AddNoPublisherHandler(
+		"test",
+		topic,
+		subscriber,
+		func(msg *message.Message) error {
+			payload := string(msg.Payload)
+			// MySQL and PostgreSQL format JSON with spaces, so we need to check both variants
+			if payload == `{"error":true}` || payload == `{"error": true}` {
 				return fmt.Errorf("error")
 			}
 
